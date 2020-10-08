8d5af3e3a72c311647b65a5accb55d970cc831cb
Owen <315872858@qq.com>
2020-10-08 19:54:55 +0800

    Update LoginExtension.cs

--- src/NetCore.Web.Extension/LoginExtension.cs
+++ src/NetCore.Web.Extension/LoginExtension.cs
@@ -14,10 +14,17 @@ namespace NetCore.Web.Extension
         public static async Task SignInAsync(this ControllerBase controller, Dictionary<string, string> claims, TimeSpan expire, string issuer = null, string audience = null)
         {
             var claimsIdentity = new ClaimsIdentity(claims.Select(p => new Claim(p.Key, p.Value)), CookieAuthenticationDefaults.AuthenticationScheme);
-            var prop = new AuthenticationProperties();
+            var prop = new AuthenticationProperties
+            {
+                AllowRefresh = true,
+                ExpiresUtc = DateTimeOffset.UtcNow.Add(expire),
+
+                IsPersistent = true
+            };
             prop.Items.Add("issuer", issuer);
             prop.Items.Add("audience", audience);
             prop.Parameters.Add("expire", expire);
+
             await controller.HttpContext.SignInAsync(
                 CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), prop);
         }
